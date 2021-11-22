31950e8ff4ab46e13edb86faed176a547c74ef07
DougAtIBM <douglas.burgman@se.ibm.com>
2021-11-22 17:59:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 ##############################################################################
 # 211122: Added definition for creating a resource group
+# 211122: added the provider block as the error message hints it need to be in all files
 ##############################################################################
 
+##############################################################################
+# IBM Cloud Provider
+##############################################################################
+
+provider ibm {
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = var.region
+  ibmcloud_timeout      = 60
+}
+
+##############################################################################
 
 ##############################################################################
 # Resource Group where resources will be created
